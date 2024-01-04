3ea2712f8b684d720c649c2c5fddb35931c73836
mustafadegirmenci <m.dgr@yahoo.com>
2024-01-04 15:37:09 +0300

    Make tcp server multithreaded

--- tcpclient.py
+++ tcpclient.py
@@ -1,7 +1,10 @@
 import socket
 
+SERVER_HOST = '172.17.0.2'
+SERVER_PORT = 8000
 
-def receive_file(server_socket, file_path):
+
+def receive_single_file(server_socket, file_path):
     with open(file_path, 'wb') as file:
         while True:
             data = server_socket.recv(1024)
@@ -9,24 +12,26 @@ def receive_file(server_socket, file_path):
                 file.write(data[:-3])
                 break
             file.write(data)
+    print(f"[INFO] Finished receiving file: {file_path}")
 
 
-def tcp_client(host, port):
+def request_files():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((host, port))
+    client_socket.connect((SERVER_HOST, SERVER_PORT))
+    print(f"[INFO] Connected to server: {SERVER_HOST}:{SERVER_PORT}.")
 
-    print(f"Connected to server {host}:{port}")
+    large_file_path = 'received_large_file'
+    receive_single_file(client_socket, large_file_path)
+    print(f"[INFO] Received large file: {large_file_path}.")
 
-    large_file = 'received_large_file'
-    receive_file(client_socket, large_file)
-
-    small_file = 'received_small_file'
-    receive_file(client_socket, small_file)
+    small_file_path = 'received_small_file'
+    receive_single_file(client_socket, small_file_path)
+    print(f"[INFO] Received small file: {small_file_path}.")
 
     client_socket.close()
+    print(f"[INFO] Connection closed.")
 
 
 if __name__ == "__main__":
-    HOST = '172.17.0.2'
-    PORT = 8000
-    tcp_client(HOST, PORT)
+    request_files()
+    print(f"[INFO] Requesting files from the server.")
--- tcpserver.py
+++ tcpserver.py
@@ -1,36 +1,45 @@
 import socket
+import threading
+
+SERVER_HOST = '172.17.0.2'
+SERVER_PORT = 8000
+BACKLOG_LIMIT = 5
 
 
-def send_file(client_socket, file_path):
+def send_single_file(client_socket, file_path):
     with open(file_path, 'rb') as file:
         for data in file:
             client_socket.sendall(data)
     client_socket.sendall(b"EOF")
+    print(f"[INFO] Finished sending file: {file_path}")
 
 
-def tcp_server(host, port):
+def handle_single_client(client_socket, addr):
+    file_to_send = '../root/objects/large-0.obj'
+    send_single_file(client_socket, file_to_send)
+    print(f"[INFO] Sending large file: {file_to_send}")
+
+    file_to_send = '../root/objects/small-0.obj'
+    send_single_file(client_socket, file_to_send)
+    print(f"[INFO] Sending small file: {file_to_send}")
+
+    client_socket.close()
+    print(f"[INFO] Connection closed for {addr}")
+
+
+def respond_file_requests():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    server_socket.listen(1)
-
-    print(f"Server listening on {host}:{port}")
+    server_socket.bind((SERVER_HOST, SERVER_PORT))
+    server_socket.listen(BACKLOG_LIMIT)
+    print(f"[INFO] Server listening on {SERVER_HOST}:{SERVER_PORT}.")
 
     while True:
-        client_socket, addr = server_socket.accept()
-        print(f"Connection established from {addr}")
-
-        file_to_send = '../root/objects/large-0.obj'
-        print(f"Sending file: {file_to_send}")
-        send_file(client_socket, file_to_send)
-
-        file_to_send = '../root/objects/small-0.obj'
-        print(f"Sending file: {file_to_send}")
-        send_file(client_socket, file_to_send)
-
-        client_socket.close()
+        client_socket, client_address = server_socket.accept()
+        thread = threading.Thread(target=handle_single_client, args=(client_socket, client_address))
+        print(f"[INFO] Client connected. {client_address}")
+        thread.start()
 
 
 if __name__ == "__main__":
-    HOST = '172.17.0.2'
-    PORT = 8000
-    tcp_server(HOST, PORT)
+    respond_file_requests()
+    print(f"[INFO] Server started.")
