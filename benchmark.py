d2d520b3087c811569bc765c2f61b1f05cfa7b25
mustafadegirmenci <m.dgr@yahoo.com>
2024-01-04 19:49:20 +0300

    Add benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,6 +1,8 @@
 import tc
 import tcpclient
 
+PACKET_DELAY_JITTER = 5
+
 if __name__ == "__main__":
     print(f"[INFO] Starting benchmark...\n")
 
@@ -8,23 +10,38 @@ if __name__ == "__main__":
     tc.clear_rules()
     elapsed_time_no_rules = tcpclient.request_files_and_measure_time(10)
 
-    print(f"[INFO] Testing with 0% packet loss...\n")
-    tc.clear_rules()
-    tc.apply_packet_loss(0)
-    elapsed_time_packet_loss_0 = tcpclient.request_files_and_measure_time(10)
+    elapsed_time_packet_loss = {}
+    for loss in [0, 5, 10, 15]:
+        print(f"[INFO] Testing with {loss}% packet loss...\n")
+        tc.clear_rules()
+        tc.apply_packet_loss(loss)
+        elapsed_time_packet_loss[loss] = tcpclient.request_files_and_measure_time(requested_file_count=10)
 
-    print(f"[INFO] Testing with 5% packet loss...\n")
-    tc.clear_rules()
-    tc.apply_packet_loss(5)
-    elapsed_time_packet_loss_5 = tcpclient.request_files_and_measure_time(10)
+    elapsed_time_packet_corruption = {}
+    for corruption in [0, 5, 10]:
+        print(f"[INFO] Testing with {corruption}% packet corruption...\n")
+        tc.clear_rules()
+        tc.apply_packet_corruption(corruption)
+        elapsed_time_packet_corruption[corruption] = tcpclient.request_files_and_measure_time(requested_file_count=10)
 
-    print(f"[INFO] Testing with 10% packet loss...\n")
+    print(f"[INFO] Testing with {100}ms packet delay and uniform distribution...\n")
     tc.clear_rules()
-    tc.apply_packet_loss(10)
-    elapsed_time_packet_loss_10 = tcpclient.request_files_and_measure_time(10)
+    tc.apply_packet_delay_uniform(100, PACKET_DELAY_JITTER)
+    elapsed_time_packet_delay_uniform = tcpclient.request_files_and_measure_time(requested_file_count=10)
 
-    print(f"[INFO] Testing with 15% packet loss...\n")
+    print(f"[INFO] Testing with {100}ms packet delay and normal distribution...\n")
     tc.clear_rules()
-    tc.apply_packet_loss(15)
-    elapsed_time_packet_loss_15 = tcpclient.request_files_and_measure_time(10)
+    tc.apply_packet_delay_normal(100, PACKET_DELAY_JITTER)
+    elapsed_time_packet_delay_normal = tcpclient.request_files_and_measure_time(requested_file_count=10)
 
+    print(f"[INFO] Benchmark Results:\n")
+    print(f"No Rules: \t{elapsed_time_no_rules}\n")
+
+    for loss in elapsed_time_packet_loss.keys():
+        print(f"Packet Loss ({loss}%): \t{elapsed_time_packet_loss[loss]}\n")
+
+    for corruption in elapsed_time_packet_corruption.keys():
+        print(f"Packet Corruption ({corruption}%): \t{elapsed_time_packet_corruption[corruption]}\n")
+
+    print(f"Delay (Uniform): \t{elapsed_time_packet_delay_uniform}\n")
+    print(f"Delay (Normal): \t{elapsed_time_packet_delay_normal}\n")
