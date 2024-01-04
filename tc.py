530f2f40f17ec0b1b43b3b4b143f3d1ad83460de
mustafadegirmenci <m.dgr@yahoo.com>
2024-01-04 19:06:06 +0300

    Add tc functions

--- tc.py
+++ tc.py
@@ -9,15 +9,15 @@ def apply_packet_loss(loss_percentage, interface=DEFAULT_INTERFACE):
     subprocess.run(command, shell=True)
 
 
-def apply_packet_delay_normal(delay_amount, interface=DEFAULT_INTERFACE):
-    print(f"[INFO] Setting packet delay to {delay_amount}ms with normal distribution...\n")
-    command = f"tc qdisc add dev {interface} root netem delay {delay_amount} distribution normal"
+def apply_packet_delay_normal(delay, jitter, interface=DEFAULT_INTERFACE):
+    print(f"[INFO] Setting packet delay to {delay}ms {jitter}ms with normal distribution...\n")
+    command = f"tc qdisc add dev {interface} root netem delay {delay} distribution normal"
     subprocess.run(command, shell=True)
 
 
-def apply_packet_delay_uniform(delay_amount, interface=DEFAULT_INTERFACE):
-    print(f"[INFO] Setting packet delay to {delay_amount}ms with uniform distribution...\n")
-    command = f"tc qdisc add dev {interface} root netem delay {delay_amount} distribution uniform"
+def apply_packet_delay_uniform(delay, jitter, interface=DEFAULT_INTERFACE):
+    print(f"[INFO] Setting packet delay to {delay}ms {jitter}ms with uniform distribution...\n")
+    command = f"tc qdisc add dev {interface} root netem delay {delay} distribution uniform"
     subprocess.run(command, shell=True)
 
 
@@ -36,10 +36,10 @@ def show_active_rules():
 apply_packet_loss(20)
 show_active_rules()
 
-apply_packet_delay_normal(20)
+apply_packet_delay_normal(100, 0)
 show_active_rules()
 
-apply_packet_delay_uniform(20)
+apply_packet_delay_uniform(100, 0)
 show_active_rules()
 
 clear_rules()
