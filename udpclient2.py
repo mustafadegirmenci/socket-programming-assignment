62ca0b0f2ddc5e69be44ff17d61878fe5ec92188
mustafadegirmenci <m.dgr@yahoo.com>
2024-01-05 18:26:52 +0300

    Make udp server always running

--- udpclient2.py
+++ udpclient2.py
@@ -38,6 +38,7 @@ def receive_single_file(sock, file_name):
                     checksum_and_data, _ = rdt_rcv(sock)
                 if checksum.validate_checksum(checksum_and_data):
                     file.write(checksum.extract_data(checksum_and_data))
+                    print(f"[INFO] Sending ACK{packet_index}.")
                     rdt_send(sock, f"ACK{packet_index}", (SERVER_IP, SERVER_PORT))
                     ack_timeout = False
                     packet_index += 1
