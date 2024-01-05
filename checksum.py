b65ca8efcbd06a7b1c4ea62112a35465d56de659
mustafadegirmenci <m.dgr@yahoo.com>
2024-01-05 17:30:30 +0300

    Remove fake ack corruption

--- checksum.py
+++ checksum.py
@@ -5,12 +5,9 @@ CHECKSUM_LENGTH = 32
 
 
 def calculate_checksum(data: bytes) -> bytes:
-    if random.random() < 0.1:
-        return hashlib.sha256(b"faulty data").digest()
-    else:
-        hash_function = hashlib.sha256()
-        hash_function.update(data)
-        return hash_function.digest()
+    hash_function = hashlib.sha256()
+    hash_function.update(data)
+    return hash_function.digest()
 
 
 def validate_checksum(checksum_and_data: bytes) -> bool:
