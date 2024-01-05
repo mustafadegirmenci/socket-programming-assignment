2bcf81cce5ed64558d03241e1a9dd1932980e686
mustafadegirmenci <m.dgr@yahoo.com>
2024-01-05 18:23:54 +0300

    Make udp server always running

--- udpclient2.py
+++ udpclient2.py
@@ -21,6 +21,8 @@ def rdt_rcv(sock) -> (bytes, (str, int)):  # data, (ip, port)
 
 
 def receive_single_file(sock, file_name):
+    sock.settimeout(TIMEOUT)
+
     checksum_and_data, _ = rdt_rcv(sock)
 
     packet_count = int(checksum_and_data.decode().split(":")[1])
@@ -31,19 +33,15 @@ def receive_single_file(sock, file_name):
     with open(file_path, "wb") as file:
         ack_timeout = False
         while packet_index < packet_count:
-            print("[INFO] Waiting for next packet...")
-            sock.settimeout(TIMEOUT)
             try:
                 if not ack_timeout:
                     checksum_and_data, _ = rdt_rcv(sock)
                 if checksum.validate_checksum(checksum_and_data):
                     file.write(checksum.extract_data(checksum_and_data))
-                    print(f"[INFO] Sending ACK{packet_index} for file {file_name}...")
                     rdt_send(sock, f"ACK{packet_index}", (SERVER_IP, SERVER_PORT))
                     ack_timeout = False
                     packet_index += 1
                 else:
-                    print(f"[INFO] Sending NAK{packet_index} for file {file_name}.........")
                     rdt_send(sock, f"NAK{packet_index}", (SERVER_IP, SERVER_PORT))
             except socket.timeout:
                 ack_timeout = True
