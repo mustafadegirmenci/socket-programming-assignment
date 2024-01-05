65b1105ae890dec63aba2d7a31ca6ce01833903d
mustafadegirmenci <m.dgr@yahoo.com>
2024-01-05 19:20:44 +0300

    Make udp server always running

--- udpclient2.py
+++ udpclient2.py
@@ -37,23 +37,22 @@ def receive_single_file(sock, file_name):
 
     packet_index = 0
     with open(file_path, "wb") as file:
-        ack_timeout = False
         while packet_index < packet_count:
-            try:
-                if not ack_timeout:
-                    checksum_and_data, _ = rdt_rcv(sock)
-                if checksum.validate_checksum(checksum_and_data):
-                    if not ack_timeout:
-                        file.write(checksum.extract_data(checksum_and_data))
-                    rdt_send(sock, f"ACK{packet_index}", (SERVER_IP, SERVER_PORT))
-                    ack_timeout = False
-                else:
-                    rdt_send(sock, f"NAK{packet_index}", (SERVER_IP, SERVER_PORT))
-            except socket.timeout:
-                ack_timeout = True
-                print(f"[INFO] ACK{packet_index} could not be sent in {TIMEOUT} seconds. Resending the ACK.")
-                continue
-            packet_index += 1
+            sock.timeout(None)
+            checksum_and_data, _ = rdt_rcv(sock)
+            sock.timeout(TIMEOUT)
+            if checksum.validate_checksum(checksum_and_data):
+                file.write(checksum.extract_data(checksum_and_data))
+
+                while True:
+                    try:
+                        rdt_send(sock, f"ACK{packet_index}", (SERVER_IP, SERVER_PORT))
+                        packet_index += 1
+                        break
+                    except socket.timeout:
+                        continue
+            else:
+                rdt_send(sock, f"NAK{packet_index}", (SERVER_IP, SERVER_PORT))
 
     print(f"[INFO] Received file {file_name}.\n")
 
